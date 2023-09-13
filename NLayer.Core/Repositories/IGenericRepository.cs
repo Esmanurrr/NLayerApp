a281f60a2855b17f1e52f43d7b87d05af6dbef27
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-09-13 16:17:38 +0300

    added GenericRepository

--- NLayer.Core/Repositories/IGenericRepository.cs
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -13,7 +13,7 @@ namespace NLayer.Core.Repositories
         IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
         IQueryable<T> Where(Expression<Func<T,bool>> expression);
         Task AddAsync(T entity);
-        Task AddRange(IEnumerable<T> entities);
+        Task AddRangeAsync(IEnumerable<T> entities);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
         void Update(T entity);
         void Remove(T entity);
