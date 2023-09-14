0dda9a88961303582f03c50ada5a8e917e4f9cf5
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-09-14 14:14:43 +0300

    added services

--- NLayer.Core/Services/IService.cs
+++ NLayer.Core/Services/IService.cs
@@ -12,8 +12,8 @@ namespace NLayer.Core.Services
         Task<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
-        Task AddAsync(T entity);
-        Task AddRange(IEnumerable<T> entities);
+        Task<T> AddAsync(T entity);
+        Task<IEnumerable<T>> AddRange(IEnumerable<T> entities);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
         Task UpdateAsync(T entity);//veri tabanına savechangeasync ile yansıtacağım için async yaptık
         Task RemoveAsync(T entity);
