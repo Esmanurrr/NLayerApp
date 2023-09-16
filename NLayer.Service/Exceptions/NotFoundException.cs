56dd3c1da1034b1a0dd8d9bcde26362724ab095d
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-09-16 07:42:32 +0300

    throw exception to service layer

---
+++ NLayer.Service/Exceptions/NotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+                
+        }
+    }
+}
